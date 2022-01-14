0767beda03c8913cefb99a5d0f280f69c8f18fc7
yuanpeipei <yuanpeipei@bitnei.cn>
2022-01-14 11:52:14 +0800

    lint test

--- src/pages/lint/index.js
+++ src/pages/lint/index.js
@@ -3,6 +3,7 @@ import '../../App.css';
 import Component from '@/component/index';
 function Lint() {
     a = 1;
+    b = 1;
     return (
         <div className="demo">
             <h4>lint</h4>
