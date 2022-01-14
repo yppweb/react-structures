37b3ad8d4e4c098393daab255fdd5f60c49eb8fd
yuanpeipei <yuanpeipei@bitnei.cn>
2022-01-14 11:50:39 +0800

    lint

--- src/pages/lint/index.js
+++ src/pages/lint/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../App.css';
 import Component from '@/component/index';
 function Lint() {
+    a = 1;
     return (
         <div className="demo">
             <h4>lint</h4>
